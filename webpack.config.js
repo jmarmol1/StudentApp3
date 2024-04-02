724daa9bad7386139dad8eaf2750e9ddcd337993
Juan Marmolejo <jmarmol1@my.centennialcollege.ca>
2024-04-01 20:19:10 -0400

    Change remote url

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = (_, argv) => ({
       name: "StudentApp3",
       filename: "remoteEntry.js",
       remotes:{
-        'StudentApp1': 'StudentApp1@http://localhost:8081/remoteEntry.js'
+        'StudentApp1': 'StudentApp1@https://student-app1.vercel.app/remoteEntry.js'
       },
       exposes: {},
       shared: {
